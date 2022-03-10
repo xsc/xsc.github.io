0c28228fd3e5bc12c3109525d7f857b6c77fe48e
Yannick Scherer <yannick.scherer@futurice.com>
2022-03-10 11:19:20 +0100

    Update portfolio data

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,71 +1,57 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  title: 'xsc.dev',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Yannick',
+  role: 'friendly technologist',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+  'Team lead and software engineer. 10+ years of experience with backend platforms and infrastructure, including web-facing and high-load environments. Strong affinity towards functional programming, TDD and free software. Strives for simplicity and intuitive interfaces. Vim user.',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/yannickscherer/',
+    github: 'https://github.com/xsc'
   },
 }
 
 const projects = [
-  // projects can be added an removed
-  // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'into-docker',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Unified and robust Docker builds without ever writing a Dockerfile. Build directly from your source code!',
+    stack: ['Clojure', 'GraalVM'],
+    sourceCode: 'https://github.com/into-docker/into-docker',
+    // livePreview: 'https://github.com',
   },
   {
-    name: 'Project 2',
+    name: 'clojure libraries',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Open-source utilities and tools, e.g. for updating oudated dependencies, rewriting Clojure code, data access, etc...',
+    stack: ['Clojure'],
+    sourceCode: 'https://github.com/xsc?tab=repositories&q=&type=&language=clojure&sort=stargazers',
   },
-  {
-    name: 'Project 3',
+{
+    name: 'aufi',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'An image upload, delivery and resizing service, based on S3 and delivering hundreds of images per second.',
+    stack: ['Clojure', 'AWS'],
+    sourceCode: 'https://github.com/stalefruits/aufi',
   },
 ]
 
 const skills = [
-  // skills can be added or removed
-  // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
-  'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Clojure', 'TypeScript', 'Java',
+  'DevOps',
+  'API Design', 'Architecture', 'Databases',
+  'Coaching & Mentoring', 'Developer Experience',
+  'AWS', 'Kubernetes', 'Docker'
+
 ]
 
 const contact = {
-  // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'yannick@xsc.dev',
 }
 
 export { header, about, projects, skills, contact }
